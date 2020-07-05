262e69f38926c15ff0304792675a5bf11ff2e76a
ruslanold <gundertajlo260@gmail.com>
2020-07-05 16:22:22 +0300

    delete file

--- dd.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define('DRUPAL_ROOT', getcwd());
-  require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-  require_once DRUPAL_ROOT . '/includes/password.inc';
-  drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-  echo user_hash_password('restailing');
-  die();
-?>
